6e74dc10b7715c17eabe3b465d302806d262bc50
kelempiau <gianandraathallahkent@gmail.com>
2025-04-30 13:47:13 +0700

    done

--- script.js
+++ script.js
@@ -3,14 +3,10 @@ window.onload = function() {
         document.getElementById('popup').classList.add('show');
     }, 800);
 
-    window.addEventListener('scroll', () => {
-        const wish = document.getElementById('slideIn');
-        const rect = wish.getBoundingClientRect();
-        if (rect.top < window.innerHeight) {
-            wish.classList.add('slide-in-active');
-        }
-    });
+    // NONAKTIFKAN animasi slide
+    // document.getElementById('slideIn').classList.add('slide-in-active');
 };
+
 
 function closePopup() {
     document.getElementById('popup').classList.remove('show');
